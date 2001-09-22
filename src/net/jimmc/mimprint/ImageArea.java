5654ce796e1e068aa3e16c4666710f03e1d7c3ec
jimmc <jimmc>
2001-09-22 04:52:37 +0000

    A minimal working implementation.

--- src/net/jimmc/mimprint/ImageArea.java
+++ src/net/jimmc/mimprint/ImageArea.java
@@ -6,125 +6,146 @@
 package jimmc.jiviewer;
 
 import java.awt.Dimension;
-import java.awt.Graphics;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.awt.Image;
-import java.awt.image.ImageObserver;
-import java.awt.Toolkit;
 import java.io.File;
-import javax.swing.JPanel;
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
 
 /** A window in which we view our images.
  */
-public class ImageArea extends JPanel implements ImageObserver {
+public class ImageArea extends JLabel implements KeyListener {
 	/** Our App. */
 	protected App app;
 
-	/** True if we need to get the width of the image. */
-	protected boolean needWidth;
+	/** The current full-size image */
+	protected Image fullSizeImage;
 
-	/** True if we need to get the height of the image. */
-	protected boolean needHeight;
-
-	/** True if we need to get the image bits of the image. */
-	protected boolean needImage;
-
-	/** The displayed image. */
-	protected Image image;
-
-	/** The width of the displayed image. */
-	protected int imageWidth;
-
-	/** The height of the displayed image. */
-	protected int imageHeight;
+	/** The current image rotation in units of 90 degrees */
+	protected int currentRotation;
 
 	/** Create an ImageArea. */
 	public ImageArea(App app) {
 		super();
 		this.app = app;
 		setPreferredSize(new Dimension(800,600));
+		setHorizontalAlignment(CENTER);
+		addKeyListener(this);
 	}
 
 	/** Show the contents of the specified image file.
 	 */
 	public void showFile(File file) {
-		needHeight = true;
-		needWidth = true;
-		needImage = true;
-		findImage(file);
-		repaint();
-	}
-
-	/** ImageObserver. */
-	public boolean imageUpdate(Image img, int infoflags,
-			int x,int y, int width, int height) {
-		if ((infoflags & ImageObserver.WIDTH)!=0) {
-			needWidth = false;
-			this.imageWidth = width;
-		}
-		if ((infoflags & ImageObserver.HEIGHT)!=0) {
-			needHeight = false;
-			this.imageHeight = height;
-		}
-		if ((infoflags & ImageObserver.ALLBITS)!=0) {
-			needImage = false;
-			synchronized(this) {
-				notifyAll();	//wake up the painter
-			}
-		}
-		return (needWidth || needHeight || needImage);
-	}
-
-	/** Paint our image. */
-	public void paint(Graphics g) {
-		g.clearRect(0,0,getWidth(),getHeight()); //clear the window
-		if (image==null)
-			return;		//nothing to draw
-		prepareImage();
-		g.drawImage(image,0,0,null);	//TBD - center in window
-	}
-
-	/** Prepare our image for drawing. */
-	protected synchronized void prepareImage() {
-		Toolkit tk = getToolkit();
-		boolean t = tk.prepareImage(image,-1,-1,this);
-System.out.println("tk.prepareImage returns "+t);
-		if (t) {
-			needImage = false;
-			return;		//got it
-		}
-		try {
-			wait(100);	//wait for it just a bit
-		} catch (InterruptedException ex) {} //ignore interrupt
-	}
-
-	/** Get the new image. */
-	protected void findImage(File file) {
-		Toolkit tk = getToolkit();
+		currentRotation = 0;
 		if (file==null) {
-			needImage = false;
-			needWidth = false;
-			needHeight = false;
-			image = null;
-			return;
+System.out.println("showFile null file");
+			setIcon(null);
+			return;		//nothing there
 		}
-		String fileName = file.getAbsolutePath();
-System.out.println("load image file "+fileName);
-		image = tk.createImage(fileName);
-		if (image==null) {
-System.out.println("no image");
-			//No image
-			needImage = false;
-			needWidth = false;
-			needHeight = false;
-			return;
+		String path = file.getAbsolutePath();
+//System.out.println("showFile "+path);
+		if (path==null) {
+			setIcon(null);
+			return;		//nothing there
 		}
-		imageWidth = image.getWidth(this);
-		if (imageWidth>0)
-			needWidth = false;
-		imageHeight = image.getHeight(this);
-		if (imageHeight>0)
-			needHeight = false;
+		PImageIcon ii = new PImageIcon(file.getAbsolutePath());
+		fullSizeImage = ii.getImage();
+		ii.pLoadImage(fullSizeImage);		//load the whole image
+		showCurrentImage();	//rotate, scale, and display
 	}
+
+	/** Redisplay the current image. */
+	public void showCurrentImage() {
+		//TBD - rotate the image first
+		Image scaledImage = getScaledImage(fullSizeImage);
+		PImageIcon ii = new PImageIcon(scaledImage);
+		ii.pLoadImage(scaledImage);	//wait for it
+		setIcon(ii);
+	}
+
+	/** Rotate the current image an additional 90 degrees counter CW */
+	public void rotate() {
+		currentRotation++;
+		if (currentRotation>=4)
+			currentRotation = 0;
+System.out.println("rotation is now "+currentRotation);
+		showCurrentImage();
+	}
+
+	/** Get a scaled version of the given image, which fits into
+	 * our window at maximum size.
+	 */
+	protected Image getScaledImage(Image sourceImage) {
+		int srcWidth = sourceImage.getWidth(null);
+		int srcHeight = sourceImage.getHeight(null);
+		int winWidth = getWidth();
+		int winHeight = getHeight();
+		if (srcWidth==winWidth && srcHeight==winHeight)
+			return sourceImage;	//exact match
+		double widthRatio = ((double)winWidth)/((double)srcWidth);
+		double heightRatio = ((double)winHeight)/((double)srcHeight);
+		int dstWidth;
+		int dstHeight;
+		if (widthRatio<heightRatio) {
+			dstWidth = winWidth;
+			dstHeight = (int)(srcHeight * widthRatio);
+		} else {
+			dstWidth = (int)(srcWidth * heightRatio);
+			dstHeight = winHeight;
+		}
+		Image scaledImage = sourceImage.getScaledInstance(
+						dstWidth,dstHeight,0);
+		return scaledImage;
+	}
+
+	class PImageIcon extends ImageIcon {
+		public PImageIcon(Image im) {
+			super(im);
+		}
+		public PImageIcon(String path) {
+			super(path);
+		}
+		public void pLoadImage(Image im) {
+			loadImage(im);
+		}
+	}
+
+	/** We return true to allow keyboard focus and thus input. */
+	public boolean isFocusTraversable() {
+		return true;	//allow keyboard input
+	}
+
+    //The KeyListener interface
+    	public void keyPressed(KeyEvent ev) {
+		int keyCode = ev.getKeyCode();
+System.out.println("keyPressed "+keyCode);
+		switch (keyCode) {
+		case KeyEvent.VK_DOWN:
+			System.out.println("DOWN");
+			break;
+		case KeyEvent.VK_UP:
+			System.out.println("UP");
+			break;
+		default:	//ignore
+		}
+	}
+	public void keyReleased(KeyEvent ev) {
+		int keyCode = ev.getKeyCode();
+System.out.println("keyReleased "+keyCode);
+	}
+	public void keyTyped(KeyEvent ev) {
+		char ch = ev.getKeyChar();
+System.out.println("keyTyped "+ch);
+		switch (ch) {
+		case 'r': case 'R':	//rotate
+			rotate();
+			break;
+		//TBD - add help popup
+		default:		//do nothing
+		}
+	}
+    //End KeyListener interface
 }
 
 /* end */
