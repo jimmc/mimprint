533dd600ae18eeb86e9550c007ab226171bd22f9
jimmc <jimmc>
2001-09-25 04:15:40 +0000

    Collection of info about an image.

---
+++ src/net/jimmc/mimprint/ImageBundle.java
@@ -0,0 +1,70 @@
+/* ImageBundle.java
+ *
+ * Jim McBeath, September 24, 2001
+ */
+
+package jimmc.jiviewer;
+
+import java.awt.Image;
+import java.io.File;
+
+/** An image in multiple sizes, plus additional information.
+ */
+public class ImageBundle {
+	/** The ImageArea in which our image will be displayed. */
+	protected ImageArea imageArea;
+
+	/** The path to this image. */
+	protected String path;
+
+	/** The original image. */
+	protected Image image;
+
+	/** The scaled image. */
+	protected Image scaledImage;
+
+	/** The index of this image in the containing list. */
+	protected int listIndex;
+
+	/** Create an image bundle for a file.
+	 * @param c A reference component.
+	 * @param file The file containing the image.
+	 * @param listIndex The list index of the file.
+	 */
+	public ImageBundle(ImageArea imageArea, File file, int listIndex) {
+		this.imageArea = imageArea;
+		path = file.getAbsolutePath();
+		image = imageArea.getToolkit().createImage(path);
+		this.listIndex = listIndex;
+	}
+
+	/** Get the path for our original image. */
+	public String getPath() {
+		return path;
+	}
+	
+	/** Get the original image from this bundle. */
+	public Image getImage() {
+		return image;
+	}
+
+	/** Get the scaled image from this bundle. */
+	public Image getScaledImage() {
+		return scaledImage;
+	}
+
+	/** Get our list index. */
+	public int getListIndex() {
+		return listIndex;
+	}
+
+	/** Load the scaled version of our image. */
+	public void loadScaledImage() {
+		imageArea.loadCompleteImage(image);
+		Image si = imageArea.getScaledImage(image);
+		imageArea.loadCompleteImage(si);
+		scaledImage = si;
+	}
+}
+
+/* end */
