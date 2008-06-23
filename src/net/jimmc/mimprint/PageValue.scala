b6d7b99717d48730b83196c69db1b6ce10e0b77a
Jim McBeath <jmcbeath@flycatcher.(none)>
2008-06-22 17:49:13 -0700

    Convert PageValue from java to scala

---
+++ src/net/jimmc/mimprint/PageValue.scala
@@ -0,0 +1,46 @@
+/* PageValue.scala
+ *
+ * Jim McBeath, June 21, 2008
+ */
+
+package net.jimmc.mimprint
+
+import java.text.NumberFormat
+
+object PageValue {
+    private var pageValueFormat:NumberFormat = _
+
+    val UNIT_MULTIPLIER = 1000
+
+    private def initPageValueFormat() {
+        if (pageValueFormat==null) {
+            pageValueFormat = NumberFormat.getNumberInstance()
+            pageValueFormat.setMaximumFractionDigits(3)
+        }
+    }
+
+    def formatPageValue(n:int):String = {
+        initPageValueFormat()
+        val d:Double = n.asInstanceOf[Double]/UNIT_MULTIPLIER
+        pageValueFormat.format(d)
+    }
+
+    def parsePageValue(s:String):Int = {
+        if (s==null) {
+            //SAX eats our exceptions and doesn't print out the trace,
+            //so we print it out here before returning
+            val ex = new NullPointerException("No value for parsePageValue")
+            ex.printStackTrace()
+            throw ex
+        }
+        val d = java.lang.Double.parseDouble(s)
+        (d*UNIT_MULTIPLIER).asInstanceOf[Int]
+    }
+
+    def parsePageValue(s:String, dflt:Int):Int = {
+        if (s==null)
+            dflt
+        else
+            parsePageValue(s)
+    }
+}
