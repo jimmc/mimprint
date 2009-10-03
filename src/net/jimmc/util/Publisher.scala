0f3413ff82c1726953922fd34be2ffd69d8a0a43
Jim McBeath <jimmc@nwlink.com>
2009-10-03 14:32:56 -0700

    Add a lock object to Publisher to reduce locking scope of synchronize

--- src/net/jimmc/util/Publisher.scala
+++ src/net/jimmc/util/Publisher.scala
@@ -8,27 +8,31 @@ package net.jimmc.util
 trait Publisher[E] {
     type S = (E) => Unit
     private var subscribers: List[S] = Nil
+    private object lock
+	//By using lock.synchronized rather than this.synchronized we reduce
+	//the scope of our lock from the extending object (which might be
+	//mixing us in with other classes) to just this trait.
 
     /** True if the subscriber is already in our list. */
     def isSubscribed(subscriber:S) = {
-	val subs = synchronized { subscribers }
+	val subs = lock.synchronized { subscribers }
 	subs.exists(_==subscriber)
     }
 
     /** Add a subscriber to our list if it is not already there. */
-    def subscribe(subscriber:S) = synchronized {
+    def subscribe(subscriber:S) = lock.synchronized {
         if (!isSubscribed(subscriber))
             subscribers = subscriber :: subscribers
     }
 
     /** Remove a subscriber from our list.  If not in the list, ignored. */
-    def unsubscribe(subscriber:S):Unit = synchronized {
+    def unsubscribe(subscriber:S):Unit = lock.synchronized {
         subscribers = subscribers.filter(_!=subscriber)
     }
 
     /** Publish an event to all subscribers on the list. */
     def publish(event:E) = {
-	val subs = synchronized { subscribers }
+	val subs = lock.synchronized { subscribers }
 	subs.foreach(_.apply(event))
     }
 }
