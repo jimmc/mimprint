ddd82ef1f77fb4d7bb24630c34993da439c6c46e
Jim McBeath <jimmc@nwlink.com>
2009-10-03 14:47:22 -0700

    Apply PlayListS -> PlayList name change to unit tests.

--- testsrc/net/jimmc/mimprint/TestPlayItem.scala
+++ testsrc/net/jimmc/mimprint/TestPlayItem.scala
@@ -8,53 +8,53 @@ import java.io.StringWriter
 class TestPlayItem extends Suite {
 
     def testIsFinalLine() {
-        assert(!PlayItemS.isFinalLine(""))
-        assert(!PlayItemS.isFinalLine("#comment"))
-        assert(!PlayItemS.isFinalLine("+command"))
-        assert(!PlayItemS.isFinalLine("-command"))
-        assert(PlayItemS.isFinalLine("filename.jpg"))
-        assert(PlayItemS.isFinalLine("filename.jpg"))
-        assert(PlayItemS.isFinalLine("-empty"));
+        assert(!PlayItem.isFinalLine(""))
+        assert(!PlayItem.isFinalLine("#comment"))
+        assert(!PlayItem.isFinalLine("+command"))
+        assert(!PlayItem.isFinalLine("-command"))
+        assert(PlayItem.isFinalLine("filename.jpg"))
+        assert(PlayItem.isFinalLine("filename.jpg"))
+        assert(PlayItem.isFinalLine("-empty"));
     }
 
     def testIsCommentLine() {
-        assert(PlayItemS.isCommentLine("#comment"))
-        assert(!PlayItemS.isCommentLine("not a comment"))
+        assert(PlayItem.isCommentLine("#comment"))
+        assert(!PlayItem.isCommentLine("not a comment"))
     }
 
     def testIsEmptyLine() {
-        assert(PlayItemS.isEmptyLine("-empty"))
-        assert(!PlayItemS.isEmptyLine("foo"))
+        assert(PlayItem.isEmptyLine("-empty"))
+        assert(!PlayItem.isEmptyLine("foo"))
     }
 
     def testImageInfoLine() {
-        val item1 = new PlayItemS(null,null,"foo",0)
+        val item1 = new PlayItem(null,null,"foo",0)
         assert("foo"==item1.getImageInfoLine())
         assert("foo"==item1.getFileName())
         assert(0==item1.getRotFlag())
 
-        val item2 = new PlayItemS(null,null,"foo.gif",0)
+        val item2 = new PlayItem(null,null,"foo.gif",0)
         assert("foo.gif"==item2.getImageInfoLine())
         assert("foo.gif"==item2.getFileName())
         assert(0==item2.getRotFlag())
 
-        val item3 = new PlayItemS(null,null,null,0)
+        val item3 = new PlayItem(null,null,null,0)
         assert("-empty"==item3.getImageInfoLine())
         assert(null==item3.getFileName())
         assert(0==item3.getRotFlag())
     }
 
     def testIsEmpty() {
-        val item1 = new PlayItemS(null,null,null,0)
+        val item1 = new PlayItem(null,null,null,0)
         assert(item1.isEmpty)
 
-        val item2 = new PlayItemS(null,null,"foo.gif",0)
+        val item2 = new PlayItem(null,null,"foo.gif",0)
         assert(!item2.isEmpty)
     }
 
     def testApply() {
         val lines = List("#comment1","foo.jpg")
-        val item1 = PlayItemS(lines, null)
+        val item1 = PlayItem(lines, null)
         assert("foo.jpg"==item1.getFileName)
         assert(0==item1.getRotFlag)
     }
@@ -64,7 +64,7 @@ class TestPlayItem extends Suite {
         val comments = List("#line 1","#line 2")
         val fn = "foo.jpg"
         val rot = -1
-        val item = new PlayItemS(comments,null,fn,rot)
+        val item = new PlayItem(comments,null,fn,rot)
         val out = new StringWriter()
         item.printAll(new PrintWriter(out), null)
 //There is now an extra comment about the base dir
